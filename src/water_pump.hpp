5b177a52a6432537399699f53006bdb98deac421
Paulo Cabral <paulo@systeminit.com>
2024-09-23 01:33:16 -0300

    Fix scheduling

--- src/water_pump.hpp
+++ src/water_pump.hpp
@@ -2,16 +2,19 @@
 #define IOP_LIB_SENSORS_WATER_PUMP_HPP
 
 #include <relay.hpp>
+#include <Arduino.h>
 
 namespace relay {
 class WaterPump {
     relay::Relay relay;
     std::unordered_map<Moment, iop::time::seconds> states;
+    std::optional<Moment> current;
     std::optional<Moment> next;
+    bool isNextTomorrow;
 public:
     WaterPump(const iop_hal::PinRaw pin) noexcept: relay(pin) {}
     WaterPump(relay::Relay relay) noexcept: relay(std::move(relay)) {}
-    
+
     auto begin() const noexcept -> void {
         this->relay.begin();
     }
@@ -35,16 +38,30 @@ public:
 
     auto actIfNeeded() noexcept -> bool {
         if (!this->next) return false;
-        if (Moment::now() < this->next) return false;
 
+        const auto now = Moment::now();
+        if (now < this->next && !(this->current && this->isNextTomorrow && now < this->current)) return false;
+        this->current = now;
+        this->isNextTomorrow = false;
+
+        bool found;
         std::optional<Moment> next;
+        std::optional<Moment> first;
         for (const auto & [moment, time]: this->states) {
-            if (this->next == moment) {
-                this->turnOnFor(time);
-            }
-            if (moment > *this->next && (!next || moment < *next)) {
+            if (!first) first = moment;
+
+            if (found) {
                 next = moment;
+                break;
+            } else if (this->next == moment) {
+                this->turnOnFor(time);
+                found = true;
             }
+        }
+
+        if (!next.has_value() && first) {
+            next = first;
+            this->isNextTomorrow = true;
         }
         this->next = next;
 
@@ -54,3 +71,4 @@ public:
 }
 
 #endif
+
